23da882802929f03f65d7a67f41dfbb52bdb1f0b
Lucydevonne <wambuilucie7@gmail.com>
2023-04-25 09:44:06 +0000

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,42 +1,53 @@
 #!/usr/bin/python3
-#Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
+
+"""
+Write a Python script that, using this REST API, for a given employee ID, 
+returns information about his/her TODO list progress.
+"""
+
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
     """
-    This function retrieves information about a given employee's TODO list progress.
+    Retrieve information about a given employee's TODO list progress.
 
-    Parameters:
-    employee_id (int): The ID of the employee to retrieve information for.
+    Args:
+        employee_id (int): The ID of the employee to retrieve information for.
 
     Returns:
-    None.
+        None.
 
     Raises:
-    None.
+        None.
     """
     url_base = "https://jsonplaceholder.typicode.com"
 
-    # get user info by id
-    user_response = requests.get(f"{url_base}/users/{employee_id}")
+    # Get user info by ID
+    user_url = "{}/users/{}".format(url_base, employee_id)
+    user_response = requests.get(user_url)
     user_info = user_response.json()
     employee_name = user_info['name']
 
-    # get user's to-do list by id
-    todo_response = requests.get(f"{url_base}/todos", params={"userId": employee_id})
+    # Get user's to-do list by ID
+    todo_url = "{}/todos".format(url_base)
+    todo_params = {"userId": employee_id}
+    todo_response = requests.get(todo_url, params=todo_params)
     todo_list = todo_response.json()
     total_tasks = len(todo_list)
     completed_tasks = [task for task in todo_list if task['completed']]
     num_completed_tasks = len(completed_tasks)
 
-    # print progress report
-    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+    # Print progress report
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, num_completed_tasks, total_tasks))
     for task in completed_tasks:
-        print(f"\t{task['title']}")
+        print("\t{}".format(task['title']))
 
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     get_employee_todo_progress(employee_id)
+
 
