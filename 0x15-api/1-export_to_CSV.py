aa478dadf7f2fa747763eea66273f6d21c3cd7d9
Lucydevonne <wambuilucie7@gmail.com>
2023-04-25 12:38:24 +0000

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -29,38 +29,27 @@ def get_employee_todo_progress(employee_id):
     user_url = "{}/users/{}".format(url_base, employee_id)
     user_response = requests.get(user_url)
     user_info = user_response.json()
-    employee_name = user_info['name']
     employee_username = user_info['username']
-    employee_id = user_info['id']
 
     # Get user's to-do list by ID
     todo_url = "{}/todos".format(url_base)
     todo_params = {"userId": employee_id}
     todo_response = requests.get(todo_url, params=todo_params)
     todo_list = todo_response.json()
-    total_tasks = len(todo_list)
-    completed_tasks = [task for task in todo_list if task['completed']]
-    num_completed_tasks = len(completed_tasks)
 
     # Print progress report
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, num_completed_tasks, total_tasks))
-    for task in completed_tasks:
-        print("\t{}".format(task['title']))
+        employee_id, len([task for task in todo_list if task['completed']]), len(todo_list)))
 
     # Write data in CSV
-    csv_filename = "employee_{}_todo.csv".format(employee_id)
+    csv_filename = "employee_{}.csv".format(employee_id)
     with open(csv_filename, mode='w', newline='') as csv_file:
-        fieldnames = ['userId', 'task', 'completed']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for task in todo_list:
-            writer.writerow({
-               'userId': task['userId'],
-               'task': task['title'],
-               'completed': task['completed']
-            })
+            writer.writerow([employee_id, employee_username, task['completed'], task['title']])
+
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     get_employee_todo_progress(employee_id)
+
