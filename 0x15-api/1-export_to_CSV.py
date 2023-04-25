d3305d8599fd6fbf2ed0ebec3fd47a54701e81f7
Lucydevonne <wambuilucie7@gmail.com>
2023-04-25 09:54:08 +0000

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,27 +1,51 @@
 #!/usr/bin/python3
 
+"""
+A script that uses the JSONPlaceholder API to retrieve information about a given employee's
+TODO list progress and save it to a CSV file.
+"""
+
 import csv
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
+    """
+    Retrieve information about a given employee's TODO list progress.
+
+    Args:
+        employee_id (int): The ID of the employee to retrieve information for.
+
+    Returns:
+        None.
+
+    Raises:
+        None.
+    """
     url = "https://jsonplaceholder.typicode.com"
-    employee_response = requests.get(url + "/users/" + str(employee_id))
+
+    # Retrieve employee information by ID
+    employee_response = requests.get("{}/users/{}".format(url, employee_id))
     employee_name = employee_response.json()["name"]
 
-    todo_response = requests.get(url + "/todos", params={"userId": employee_id})
+    # Retrieve employee's TODO list by ID
+    todo_response = requests.get("{}/todos".format(url), params={"userId": employee_id})
     todos = todo_response.json()
     total_tasks = len(todos)
     done_tasks = sum(1 for todo in todos if todo["completed"])
 
-    csv_filename = str(employee_id) + ".csv"
+    # Save employee TODO list to CSV file
+    csv_filename = "{}.csv".format(employee_id)
     with open(csv_filename, mode="w", newline="") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
         for todo in todos:
             writer.writerow([employee_id, employee_name, todo["completed"], todo["title"]])
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, done_tasks, total_tasks))
+    # Print progress report
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, done_tasks, total_tasks))
     for todo in todos:
         if todo["completed"]:
             print("\t{}".format(todo["title"]))
@@ -30,4 +54,3 @@ def get_employee_todo_progress(employee_id):
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     get_employee_todo_progress(employee_id)
-
