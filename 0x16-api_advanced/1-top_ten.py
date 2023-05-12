4a98d61e7a8f846a932852ea4035a2a3c3dbb5ac
Lucydevonne <wambuilucie7@gmail.com>
2023-05-12 11:27:11 +0000

    api adv

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+Queries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit.
 """
 import requests
 
@@ -16,7 +17,8 @@ def top_ten(subreddit):
         None.
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 
+                (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
