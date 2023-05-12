dc5f368042f652d17129acbdcd0c603874307eaf
Lucydevonne <wambuilucie7@gmail.com>
2023-05-12 11:21:07 +0000

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
+Queries the Reddit API and returns the no of subscribers for a given subreddit.
 If an invalid subreddit is given, the function returns 0.
 """
 
