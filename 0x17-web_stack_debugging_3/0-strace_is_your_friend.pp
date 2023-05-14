b7c80f41d2ee60315636b33e05a42c56fee7f804
lucydevonne <wambuilucie7@gmail.com>
2023-05-14 02:31:38 +0000

    debug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -7,16 +7,16 @@ package { 'strace':
 
 # Restart Apache service
 service { 'apache2':
-  ensure => running,
-  enable => true,
+  ensure  => running,
+  enable  => true,
   require => Package['strace'],
 }
 
 # Use strace to find the issue causing the 500 error and fix it
 exec { 'fix-apache':
-  command => 'strace -p $(pgrep apache2) -f -e trace=file 2>&1 | grep --color=never "/etc/apache2/"',
-  path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
-  logoutput => true,
-  notify => Service['apache2'],
+  command     => 'strace -p $(pgrep apache2) -f -e trace=file 2>&1 | grep --color=never "/etc/apache2/"',
+  path        => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+  logoutput   => true,
+  notify      => Service['apache2'],
   refreshonly => true,
 }
